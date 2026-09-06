9faad48df164ad603dd5559a41381b0477139605
Marcel Posdijk <mposdijk@marpos.nl>
2026-09-06 09:36:23 +0200

    docs: confirm published 0.3.0 installation

--- apps/docs/shared/documentation.ts
+++ apps/docs/shared/documentation.ts
@@ -3,5 +3,5 @@ export const documentationDefaults = {
   docsRepositoryBranch: 'master',
   feedbackRepositoryUrl: 'https://github.com/ludulicious/nuxt-customer-portal',
   productRepositoryUrl: 'https://github.com/ludulicious/nuxt-customer-portal',
-  productSourceCommit: 'master'
+  productSourceCommit: 'c910d71e961798225617ea8cd4e4961166bdf20d'
 } as const
